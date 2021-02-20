0da3f6e395d9d0237ed5b9e90fe4fe371b4e6f2f
HotPotatoC <juandotulung@gmail.com>
2021-02-20 19:46:16 +0700

    feat: added pprof for debugging mode

--- cmd/kvstore_server/main.go
+++ cmd/kvstore_server/main.go
@@ -2,24 +2,48 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/HotPotatoC/kvstore/internal/server"
+	"github.com/HotPotatoC/kvstore/pkg/logger"
+	"go.uber.org/zap"
+
+	"net/http"
+	_ "net/http/pprof"
 )
 
+var log *zap.SugaredLogger
+
 var (
-	host = flag.String("host", "0.0.0.0", "KVStore server host")
-	port = flag.Int("port", 7275, "KVStore server port")
+	host  = flag.String("host", "0.0.0.0", "KVStore server host")
+	port  = flag.Int("port", 7275, "KVStore server port")
+	debug = flag.Bool("debug", false, "Debug mode")
 )
 
 func init() {
 	flag.StringVar(host, "h", "0.0.0.0", "KVStore server host")
 	flag.IntVar(port, "p", 7275, "KVStore server port")
+	flag.BoolVar(debug, "d", false, "Debug mode")
+}
+
+func init() {
+	log = logger.NewLogger()
 }
 
 func main() {
 	flag.Parse()
 
 	server := server.New()
+
+	if *debug {
+		log.Info("-=-=-=-=-=-= Running in debug mode =-=-=-=-=-=-")
+		go func() {
+			log.Infof("Pprof started -> http://%s:%d/debug/pprof", *host, *port+1)
+			if err := http.ListenAndServe(fmt.Sprintf("%s:%d", *host, *port+1), nil); err != nil {
+				log.Fatalf("pprof failed: %v", err)
+			}
+		}()
+	}
 
 	server.Start(*host, *port)
 }
