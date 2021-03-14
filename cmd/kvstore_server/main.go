34694c76ca2bd95c5abac923ca5b02de9937f448
HotPotatoC <juandotulung@gmail.com>
2021-03-14 17:38:29 +0700

    refactor: change default host from 127.0.0.1 -> 0.0.0.0

--- cmd/kvstore_server/main.go
+++ cmd/kvstore_server/main.go
@@ -20,13 +20,13 @@ var Build = "dev"
 var log *zap.SugaredLogger
 
 var (
-	host  = flag.String("host", "127.0.0.1", "KVStore server host")
+	host  = flag.String("host", "0.0.0.0", "KVStore server host")
 	port  = flag.Int("port", 7275, "KVStore server port")
 	debug = flag.Bool("debug", false, "Debug mode")
 )
 
 func init() {
-	flag.StringVar(host, "h", "127.0.0.1", "KVStore server host")
+	flag.StringVar(host, "h", "0.0.0.0", "KVStore server host")
 	flag.IntVar(port, "p", 7275, "KVStore server port")
 	flag.BoolVar(debug, "d", false, "Debug mode")
 }
