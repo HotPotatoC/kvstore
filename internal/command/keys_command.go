0caf779995c2c11d7b16ec47da079e4588b43b43
HotPotatoC <juandotulung@gmail.com>
2021-04-16 20:58:20 +0700

    refactor(command): prevent listing more than 50 items on keys and list command

--- internal/command/keys_command.go
+++ internal/command/keys_command.go
@@ -25,10 +25,15 @@ func (c keysCommand) Execute(args []string) []byte {
 	var b bytes.Buffer
 	idx := 1
 	for entry := range c.db.Iter() {
-		b.WriteString(fmt.Sprintf("%d) %s", idx, entry.Key))
-		b.WriteString("\n")
+		// Prevent from sending more than 50 keys
+		if idx > 50 {
+			b.WriteString("...\n\n")
+			break
+		}
+		b.WriteString(fmt.Sprintf("%d) %s\n", idx, entry.Key))
 		idx++
 	}
 
+	b.WriteString(fmt.Sprintf("%d keys\n", c.db.Size()))
 	return b.Bytes()
 }
--- internal/command/list_command.go
+++ internal/command/list_command.go
@@ -23,10 +23,17 @@ func (c listCommand) String() string {
 
 func (c listCommand) Execute(args []string) []byte {
 	var b bytes.Buffer
-	b.WriteString(fmt.Sprintf("%d items\n", c.db.Size()))
+	i := 0
 	for entry := range c.db.Iter() {
+		// Prevent from sending more than 50 items
+		if i > 50 {
+			b.WriteString("...\n\n")
+			break
+		}
 		b.WriteString(fmt.Sprintf("%s -> \"%s\"\n", entry.Key, entry.Value))
+		i++
 	}
 
+	b.WriteString(fmt.Sprintf("%d items\n", c.db.Size()))
 	return b.Bytes()
 }
