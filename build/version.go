f3f1c21f35ed1d0413c7caa383ab0388d26104b2
HotPotatoC <juandotulung@gmail.com>
2021-11-29 15:11:34 +0700

    feat: bump version v2.0.0-beta.3 -> v2.0.0-beta.4

--- build/version.go
+++ build/version.go
@@ -1,7 +1,7 @@
 package build
 
 // Version is the current version of the build.
-var Version = "v2.0.0-beta.3"
+var Version = "v2.0.0-beta.4"
 
 // Build is the git commit of the build.
 var Build = "dev"
